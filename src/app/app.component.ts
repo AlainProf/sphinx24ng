50085927e394fa1e12a3282be7bf7d498f66d994
alainprof <amartel@cstj.qc.ca>
2024-08-28 10:50:33 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sphinx-ng';
+
+  valeurADeviner;
+  essaiCourant:number=0;
+  nbEssais=0;
+  resultat="";
+  classeResultat="";
+
+  constructor(){
+    this.valeurADeviner=0;
+  }
+
+  demarrer()
+  {
+    this.valeurADeviner = Math.floor(1 + Math.random()*1000);
+    console.log("Nombre à deviner:" + this.valeurADeviner);
+  }
+ 
+  verifier()
+  {
+    this.nbEssais++;
+    if (this.essaiCourant < this.valeurADeviner )
+      {
+         this.resultat = "Trop petit";
+         this.classeResultat = "tropBas";
+      }
+  
+      if (this.essaiCourant > this.valeurADeviner )
+      {
+        this.resultat = "Trop haut";
+        this.classeResultat = "tropHaut";
+      }
+  
+      if (this.essaiCourant == this.valeurADeviner )
+      {
+        this.resultat = "BiINGO!!";
+        this.classeResultat = "succes";
+      }
+  }
 }
